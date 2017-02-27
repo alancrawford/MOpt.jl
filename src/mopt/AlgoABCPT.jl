4a2ef47b0be0ffd6420669eb0fcf2448d3105709
Alan Crawford <a.crawford.11@ucl.ac.uk>
2017-02-27 22:33:23 +0000

    fix typo

--- src/mopt/AlgoABCPT.jl
+++ src/mopt/AlgoABCPT.jl
@@ -232,10 +232,12 @@ function rwAdapt!(algo::MAlgoABCPT, ACC::Bool, ch::Int64)
         range = algo.i>30 ? collect(algo.i+1-30:algo.i) : collect(1:algo.i)
         algo.MChains[ch].mu = mean(convert(Array,parameters(algo.MChains[ch],range)[:, ps2s_names(algo.m)]),1)[:] 
         # Update Sampling Variance (If acceptance above long run target, set net wider by increasing variance, otherwise reduce it)
-        algo.MChains[ch].infos[algo.i,:accept_rate] = algo.i>100 ?
-               sum(algo.MChains[ch].infos[algo.i-100:algo.i,:accept])/100 :
-               sum(algo.MChains[ch].infos[1:algo.i:accept])/algo.i
-        algo.MChains[ch].shock_sd                    += step * (algo.MChains[ch].infos[algo.i,:accept_rate]- 0.234)
+        if algo.i>100
+            algo.MChains[ch].infos[algo.i,:accept_rate] = sum(algo.MChains[ch].infos[algo.i-100:algo.i,:accept])/100 
+        else
+            algo.MChains[ch].infos[algo.i,:accept_rate] = sum(algo.MChains[ch].infos[1:algo.i:accept])/algo.i
+        end
+        algo.MChains[ch].shock_sd += step * (algo.MChains[ch].infos[algo.i,:accept_rate]- 0.234)
         algo.MChains[ch].infos[algo.i,:shock_sd]      = mean(algo.MChains[ch].shock_sd)
     end
 end
