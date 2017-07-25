e2013019175f539d44505b3a4b0fac057d95ef02
Alan Crawford <a.crawford.11@ucl.ac.uk>
2017-07-25 18:12:35 +0100

    Add Bnd and switch existing commands to unbounded

--- src/mopt/GetCandidates.jl
+++ src/mopt/GetCandidates.jl
@@ -1,20 +1,4 @@
 
-# function getNewCandidates!
-function getNewCandidates!(algo::MAlgoABCPT)
-
-    # Number of parameters
-    D = length(ps2s_names(algo.m))
-
-    # update chain by chain
-    for ch in 1:algo["N"]
-        # shock parameters on chain index ch
-        shock = exp(algo.MChains[ch].shock_sd[1]).*tril(algo.MChains[ch].F)*randn(D)    # Draw shocks scaled to ensure acceptance rate targeted at 0.234 (See Lacki and Meas)
-        shockd = Dict(zip(ps2s_names(algo) , shock))             # Put in a dictionary
-        jumpParamsBnd!(algo,ch,shockd)                              # Add to parameters
-    end
-
-end
-
 # No boundaries
 function jumpParams!(algo::MAlgoABCPT,ch::Int,shock::Dict)
     eval_old = getLastEval(algo.MChains[ch])
@@ -33,6 +17,37 @@ function jumpParamsBnd!(algo::MAlgoABCPT,ch::Int,shock::Dict)
     end
 end                                                
 
+# function getNewCandidates!
+function getNewCandidates!(algo::MAlgoABCPT)
+
+    # Number of parameters
+    D = length(ps2s_names(algo.m))
+
+    # update chain by chain
+    for ch in 1:algo["N"]
+        # shock parameters on chain index ch
+        shock = exp(algo.MChains[ch].shock_sd[1]).*tril(algo.MChains[ch].F)*randn(D)    # Draw shocks scaled to ensure acceptance rate targeted at 0.234 (See Lacki and Meas)
+        shockd = Dict(zip(ps2s_names(algo) , shock))             # Put in a dictionary
+        jumpParams!(algo,ch,shockd)                              # Add to parameters
+    end
+
+end
+
+function getNewCandidatesBnd!(algo::MAlgoABCPT)
+
+    # Number of parameters
+    D = length(ps2s_names(algo.m))
+
+    # update chain by chain
+    for ch in 1:algo["N"]
+        # shock parameters on chain index ch
+        shock = exp(algo.MChains[ch].shock_sd[1]).*tril(algo.MChains[ch].F)*randn(D)    # Draw shocks scaled to ensure acceptance rate targeted at 0.234 (See Lacki and Meas)
+        shockd = Dict(zip(ps2s_names(algo) , shock))             # Put in a dictionary
+        jumpParamsBnd!(algo,ch,shockd)                              # Add to parameters
+    end
+
+end
+
 get_eigs(W::Matrix{Float64}) = svdvals(W).^2
 
 function draw_from_ppca(M::MultivariateStats.PPCA{Float64}) 
@@ -45,6 +60,66 @@ end
 
 # function getNewCandidates!
 function getNewCandidatesPPCA!(algo::MAlgoABCPT, method::Symbol)
+
+    # Number of parameters
+    D =  length(ps2s_names(algo.m))
+
+    if algo["CommonCov"]
+        S = A_mul_Bt(algo.MChains[1].F,algo.MChains[1].F)
+        if method==:em
+             M = MultivariateStats.ppcaem(S, algo.MChains[1].mu, D; maxoutdim = algo["maxoutdim"]) 
+        elseif method==:bayes
+             M = MultivariateStats.bayespca(S, algo.MChains[1].mu, D; maxoutdim = algo["maxoutdim"])
+        else 
+            println("Error: not an option for online PPCA")
+        end
+        # shock parameters on chain index ch
+        for ch in 1:algo["N"]
+            (ρbar,l_id,σ,w) = draw_from_ppca(M)
+            λ = exp(algo.MChains[ch].shock_sd[l_id])                # Scaling
+            shock = λ*σ*randn()*w                                   # Vector - shock
+            shockd = Dict(zip(ps2s_names(algo) , shock))             # Put in a dictionary
+            jumpParams!(algo,ch,shockd)                              # Add to parameters
+
+            # Records which principal component changes
+            algo.MChains[ch].shock_id = l_id
+            fill!(algo.MChains[ch].shock_wgts,0.)
+            for (n,k) in enumerate(ρbar)
+                algo.MChains[ch].shock_wgts[n] = k
+            end
+        end        
+
+    else 
+        # update chain by chain
+        for ch in 1:algo["N"]
+            S = A_mul_Bt(algo.MChains[ch].F,algo.MChains[ch].F)
+            if method==:em
+                 M = MultivariateStats.ppcaem(S, algo.MChains[ch].mu, D; maxoutdim = algo["maxoutdim"]) 
+            elseif method==:bayes
+                 M = MultivariateStats.bayespca(S, algo.MChains[ch].mu, D; maxoutdim = algo["maxoutdim"])
+            else 
+                println("Error: not an option for online PPCA")
+            end
+
+            # shock parameters on chain index ch
+            (ρbar,l_id,σ,w) = draw_from_ppca(M)
+            λ = exp(algo.MChains[ch].shock_sd[l_id])                # Scaling
+            shock = λ*σ*randn()*w                                   # Vector - shock
+            shockd = Dict(zip(ps2s_names(algo) , shock))             # Put in a dictionary
+            jumpParams!(algo,ch,shockd)                              # Add to parameters
+
+            # Records which principal component changes
+            algo.MChains[ch].shock_id = l_id
+            fill!(algo.MChains[ch].shock_wgts,0.)
+            for (n,k) in enumerate(ρbar)
+                algo.MChains[ch].shock_wgts[n] = k
+            end
+        end
+    end
+end
+
+# function getNewCandidates!
+function getNewCandidatesPPCABnd!(algo::MAlgoABCPT, method::Symbol)
 
     # Number of parameters
     D =  length(ps2s_names(algo.m))
@@ -103,9 +178,39 @@ function getNewCandidatesPPCA!(algo::MAlgoABCPT, method::Symbol)
     end
 end
 
-
 # function getNewCandidates!
 function getNewCandidatesCompWise!(algo::MAlgoABCPT)
+
+   # Number of parameters
+    D = length(ps2s_names(algo.m))
+
+    # update chain by chain
+    for ch in 1:algo["N"]
+
+        # shock parameters on chain index ch
+        l_id = rand(1:D)                            # Which component changing
+        λ = exp(algo.MChains[ch].shock_sd[l_id])    # Scaling of chosen component
+        S = A_mul_Bt(algo.MChains[ch].F,algo.MChains[ch].F)
+        σ = sqrt(S[l_id,l_id])
+
+        eval_old = getLastEval(algo.MChains[ch])
+        for (n,k) in enumerate(keys(eval_old.params))
+            if n==l_id
+                #algo.current_param[ch][k] = eval_old.params[k] + λ*σ*randn()
+                algo.current_param[ch][k] = eval_old.params[k] + λ*σ*randn()
+            else 
+                algo.current_param[ch][k] =  eval_old.params[k]
+            end
+        end
+
+        # Records which component changes
+        algo.MChains[ch].shock_id = l_id
+        fill!(algo.MChains[ch].shock_wgts,1/D)
+    end
+end
+
+# function getNewCandidates!
+function getNewCandidatesCompWiseBnd!(algo::MAlgoABCPT)
 
    # Number of parameters
     D = length(ps2s_names(algo.m))
