6b31e7785a32133a1b8e84e4c64e2546419a445b
Alan Crawford <a.crawford.11@ucl.ac.uk>
2017-08-07 19:31:37 +0100

    Change rwapadt

--- src/mopt/rwadapt.jl
+++ src/mopt/rwadapt.jl
@@ -15,27 +15,26 @@ rank1update(F::Matrix{Float64}, Σ::Matrix{Float64}, Nx::Int64, ρ::Float64) = t
 function rwAdapt!(algo::MAlgoABCPT, prob_accept::Float64, ch::Int64)
     
     Nx = length(MOpt.ps2s_names(algo.m))
-    step = (algo.i+1)^(-0.5)  # Declining step size over iterations 
+    step = (algo.i+1)^(-0.3)  # Declining step size over iterations 
 
     # Get value of accepted (i.e. old or new) parameters in chain after MH
     Xtilde = convert(Array,parameters(algo.MChains[ch],algo.i)[:, ps2s_names(algo.m)])[:]
-    dx = Xtilde - algo.MChains[ch].mu
+    
+    # Update mu
+    algo.MChains[ch].mu +=  step * ( Xtilde - algo.MChains[ch].mu )
 
     # Get Cholesky Factorisation of Covariance matrix (before update mu)
     #algo.MChains[ch].F += step*algo.MChains[ch].F*rank1update(algo.MChains[ch].F,dx,Nx)
-    if algo.i > algo["TempAdapt"]
+    if algo.i > algo["rwAdapt"]
         lower_bound_index = maximum([1,algo.i-algo["past_iterations"]])
         Σ = cov(convert(Matrix,MOpt.parameters(algo.MChains[ch],lower_bound_index:algo.i)))
         algo.MChains[ch].F = convert(Matrix,cholfact(Σ)[:L])
+        algo.MChains[ch].shock_sd += step * (prob_accept - 0.234)   # Quite a simple update - maybe be slow. See AT 2008 sec 5.   
     end
-    # Update mu
-    algo.MChains[ch].mu +=  step * dx
 
     # Update Sampling Variance (If acceptance above long run target, set net wider by increasing variance, otherwise reduce it)
     algo.MChains[ch].infos[algo.i,:accept_rate] = sum(algo.MChains[ch].infos[1:algo.i,:accept])/algo.i
    
-    # Update acceptance rate
-    algo.MChains[ch].shock_sd += step * (prob_accept - 0.234)   # Quite a simple update - maybe be slow. See AT 2008 sec 5.
     #algo.MChains[ch].shock_sd += step * (algo.MChains[ch].infos[algo.i,:accept_rate]- 0.234)
     algo.MChains[ch].infos[algo.i,:shock_sd] = algo.MChains[ch].shock_sd
 
@@ -45,29 +44,25 @@ end
 function rwAdapt!(algo::MAlgoABCPT, prob_accept::Float64, ch::Int64, ρ::Float64)
     
     Nx = length(MOpt.ps2s_names(algo.m))
-    step = (algo.i+1)^(-0.5)  # Declining step size over iterations 
+    step = (algo.i+1)^(-0.3)  # Declining step size over iterations 
 
     # Get value of accepted (i.e. old or new) parameters in chain after MH
     Xtilde = convert(Array,parameters(algo.MChains[ch],algo.i)[:, ps2s_names(algo.m)])[:]
-    dx = Xtilde - algo.MChains[ch].mu
+    algo.MChains[ch].mu +=  step *( Xtilde - algo.MChains[ch].mu )
 
     # Get Cholesky Factorisation of Covariance matrix (before update mu)
     #algo.MChains[ch].F += step*algo.MChains[ch].F*rank1update(algo.MChains[ch].F,dx,Nx,ρ)
-    if algo.i > algo["TempAdapt"]
+    if algo.i > algo["rwAdapt"]
         lower_bound_index = maximum([1,algo.i-algo["past_iterations"]])
         Σ = (1-ρ).*cov(convert(Matrix,MOpt.parameters(algo.MChains[ch],lower_bound_index:algo.i))) + ρ.*eye(Nx)
         algo.MChains[ch].F = convert(Matrix,cholfact(Σ)[:L])    
+        algo.MChains[ch].shock_sd += step * (prob_accept - 0.234)   # Quite a simple update - maybe be slow. See AT 2008 sec 5.
     end
-
-    # Update mu
-    algo.MChains[ch].mu +=  step * dx
 
     # Update Sampling Variance (If acceptance above long run target, set net wider by increasing variance, otherwise reduce it)
     algo.MChains[ch].infos[algo.i,:accept_rate] = sum(algo.MChains[ch].infos[1:algo.i,:accept])/algo.i
    
     # Update acceptance rate
-    algo.MChains[ch].shock_sd += step * (prob_accept - 0.234)   # Quite a simple update - maybe be slow. See AT 2008 sec 5.
-    #algo.MChains[ch].shock_sd += step * (algo.MChains[ch].infos[algo.i,:accept_rate]- 0.234)
     algo.MChains[ch].infos[algo.i,:shock_sd] = algo.MChains[ch].shock_sd
 
 end
@@ -84,13 +79,13 @@ function rwAdapt!(algo::MAlgoABCPT, pvec::Vector{Float64})
         Xtilde = convert(Array,parameters(algo.MChains[ch],algo.i)[:, ps2s_names(algo.m)])[:]
         dx = Xtilde - algo.MChains[ch].mu
         #Σ += A_mul_Bt(dx,dx)/algo["N"]      # Cov matrix update 
-        Δmu += dx/algo["N"]                 # Mean update
+        algo.MChains[ch].mu +=  step * (dx/algo["N"])              # Mean update
     end
 
     # Read updates into chains
-    step = (algo.i+1)^(-0.5)  # Declining step size over iterations 
+    step = (algo.i+1)^(-0.3)  # Declining step size over iterations 
     
-    if algo.i > algo["TempAdapt"]
+    if algo.i > algo["rwAdapt"]
         lower_bound_index = maximum([1,algo.i-algo["past_iterations"]])
         for ch in 1:algo["N"]
             if ch==1
@@ -101,12 +96,13 @@ function rwAdapt!(algo::MAlgoABCPT, pvec::Vector{Float64})
         end
         Σ = (1-ρ).*cov(S) + ρ.*eye(Nx)
         F = convert(Matrix,cholfact(Σ)[:L])
-        for ch in 1:algo["N"] algo.MChains[ch].F  = F end
+        for ch in 1:algo["N"] 
+            algo.MChains[ch].F  = F 
+            algo.MChains[ch].shock_sd += step * (pvec[ch] - 0.234)   # Quite a simple update - maybe be slow. See AT 2008 sec 5.
+        end
     end
     
     @inbounds for ch in 1:algo["N"]
-        algo.MChains[ch].mu +=  step * Δmu
-        algo.MChains[ch].shock_sd += step * (pvec[ch] - 0.234)   # Quite a simple update - maybe be slow. See AT 2008 sec 5.
         algo.MChains[ch].infos[algo.i,:shock_sd] = algo.MChains[ch].shock_sd
         algo.MChains[ch].infos[algo.i,:accept_rate] = sum(algo.MChains[ch].infos[1:algo.i,:accept])/algo.i  # Update acceptance rate
     end
@@ -125,12 +121,12 @@ function rwAdapt!(algo::MAlgoABCPT, pvec::Vector{Float64}, ρ::Float64)
     @inbounds for ch in 1:algo["N"]
         Xtilde = convert(Array,parameters(algo.MChains[ch],algo.i)[:, ps2s_names(algo.m)])[:]
         dx = Xtilde - algo.MChains[ch].mu
-        Δmu += dx/algo["N"]                 # Mean update
+        algo.MChains[ch].mu +=  step * (dx/algo["N"] )                # Mean update
     end
 
     # Read updates into chains
-    step = (algo.i+1)^(-0.5)  # Declining step size over iterations 
-    if algo.i > algo["TempAdapt"]
+    step = (algo.i+1)^(-0.3)  # Declining step size over iterations 
+    if algo.i > algo["rwAdapt"]
         lower_bound_index = maximum([1,algo.i-algo["past_iterations"]])
         for ch in 1:algo["N"]
             if ch==1
@@ -141,12 +137,14 @@ function rwAdapt!(algo::MAlgoABCPT, pvec::Vector{Float64}, ρ::Float64)
         end
         Σ = (1-ρ).*cov(S) + ρ.*eye(Nx)
         F = convert(Matrix,cholfact(Σ)[:L])
-        for ch in 1:algo["N"] algo.MChains[ch].F  = F end
+        for ch in 1:algo["N"] 
+            algo.MChains[ch].F  = F
+            algo.MChains[ch].shock_sd += step * (pvec[ch] - 0.234)  
+        end
     end
 
     @inbounds for ch in 1:algo["N"]
-        algo.MChains[ch].mu +=  step * Δmu
-        algo.MChains[ch].shock_sd += step * (pvec[ch] - 0.234)   # Quite a simple update - maybe be slow. See AT 2008 sec 5.
+        # Quite a simple update - maybe be slow. See AT 2008 sec 5.
         algo.MChains[ch].infos[algo.i,:shock_sd] = algo.MChains[ch].shock_sd
         #algo.MChains[ch].shock_sd += step * (algo.MChains[ch].infos[algo.i,:accept_rate]- 0.234)
         algo.MChains[ch].infos[algo.i,:accept_rate] = sum(algo.MChains[ch].infos[1:algo.i,:accept])/algo.i  # Update acceptance rate
@@ -162,7 +160,7 @@ end
 function rwAdaptLocal!(algo::MAlgoABCPT, prob_accept::Float64, ch::Int64)
     
     Nx = length(ps2s_names(algo.m))
-    step = (algo.i+1)^(-0.5)  # Declining step size over iterations 
+    step = (algo.i+1)^(-0.3)  # Declining step size over iterations 
 
     # Get value of accepted (i.e. old or new) parameters in chain after MH
     Xtilde = convert(Array,parameters(algo.MChains[ch],algo.i)[:, ps2s_names(algo.m)])[:]
@@ -190,7 +188,7 @@ end
 function rwAdaptLocal!(algo::MAlgoABCPT, prob_accept::Float64, ch::Int64, ρ::Float64)
     
     Nx = length(MOpt.ps2s_names(algo.m))
-    step = (algo.i+1)^(-0.5)  # Declining step size over iterations 
+    step = (algo.i+1)^(-0.3)  # Declining step size over iterations 
 
     # Get value of accepted (i.e. old or new) parameters in chain after MH
     Xtilde = convert(Array,parameters(algo.MChains[ch],algo.i)[:, ps2s_names(algo.m)])[:]
@@ -229,7 +227,7 @@ function rwAdaptLocal!(algo::MAlgoABCPT, pvec::Vector{Float64})
     end
 
     # Read updates into chains
-    step = (algo.i+1)^(-0.5)  # Declining step size over iterations
+    step = (algo.i+1)^(-0.3)  # Declining step size over iterations
     lower_bound_index = maximum([1,algo.i-algo["past_iterations"]])
     Σ = cov(convert(Matrix,MOpt.parameters(algo.MChains[1:algo["N"]],lower_bound_index:algo.i)))
 
@@ -260,7 +258,7 @@ function rwAdaptLocal!(algo::MAlgoABCPT, pvec::Vector{Float64},ρ::Float64)
     end
 
     # Read updates into chains
-    step = (algo.i+1)^(-0.5)  # Declining step size over iterations 
+    step = (algo.i+1)^(-0.3)  # Declining step size over iterations 
     lower_bound_index = maximum([1,algo.i-algo["past_iterations"]])
     Σ = (1-ρ).*cov(convert(Matrix,MOpt.parameters(algo.MChains[1:algo["N"]],lower_bound_index:algo.i))) + ρ.*eye(Nx)
 
